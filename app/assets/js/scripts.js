fd7c9cf12aeec2d4d4e712c53800a357308f7ebc
Max Kirchoff <max@lamasix.com>
2017-11-13 11:29:16 -0500

    initial commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * islaandchris.com
+ * Isla and Chris's wedding website.
+ * https://github.com/LamaSix/islaandchris.com
+ * @author Max Kirchoff
+ * @version 1.0.0
+ * Copyright 2017. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // App
+  });
+
+})(jQuery, window, document);
